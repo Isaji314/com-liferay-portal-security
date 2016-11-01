topolik/liferay-portal

LPS-65350 Basic Service Access Quota impression persistence API

modules/apps/foundation/portal-security/portal-security-service-access-quota-api/src/main/java/com/liferay/portal/security/service/access/quota/metric/SAQContextMatcher.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.service.access.quota.metric;
+
+import aQute.bnd.annotation.ProviderType;
+
+import java.util.Set;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@ProviderType
+public interface SAQContextMatcher {
+
+	public Set<String> getMetrics();
+
+	public boolean matches(String metric, String value);
+
+}

Isaji314/com-liferay-portal-security

portal-security-service-access-quota-api/src/main/java/com/liferay/portal/security/service/access/quota/metric/SAQContextMatcher.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.service.access.quota.metric;
+
+import aQute.bnd.annotation.ProviderType;
+
+import java.util.Set;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@ProviderType
+public interface SAQContextMatcher {
+
+	public Set<String> getMetricNames();
+
+	public boolean matches(String metricName, String metricValue);
+
+}