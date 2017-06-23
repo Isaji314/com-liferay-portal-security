suprithas/liferay-portal

LPS-72151 User Action to retreive user name and email

modules/apps/foundation/portal-security/portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/auth/publicpath/AuthPublicPath.java
@@ -23,7 +23,8 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"auth.public.path=/portal/wedeploy/access_token",
-		"auth.public.path=/portal/wedeploy/authorize"
+		"auth.public.path=/portal/wedeploy/authorize",
+		"auth.public.path=/portal/wedeploy/whoami"
 	},
 	service = Object.class
 )

modules/apps/foundation/portal-security/portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/portlet/action/WeDeployUserInfoAction.java
@@ -1,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.wedeploy.auth.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+import com.liferay.portal.kernel.struts.StrutsAction;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.security.wedeploy.auth.constants.WeDeployAuthTokenConstants;
+import com.liferay.portal.security.wedeploy.auth.service.WeDeployAuthTokenLocalService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Supritha Sundaram
+ */
+@Component(
+	immediate = true, property = {"path=/portal/wedeploy/whoami"},
+	service = StrutsAction.class
+)
+public class WeDeployUserInfoAction extends BaseStrutsAction {
+
+	@Override
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		String accessToken = ParamUtil.getString(request, "token");
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			User user = _weDeployAuthTokenLocalService.getUserByToken(
+				accessToken, WeDeployAuthTokenConstants.TYPE_AUTHORIZATION);
+
+			JSONObject elementJSONObject = JSONFactoryUtil.createJSONObject();
+
+			elementJSONObject.put("email", user.getEmailAddress());
+			elementJSONObject.put("name", user.getFullName());
+
+			jsonObject.put("info", elementJSONObject);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			jsonObject.put(
+				"error_message",
+				LanguageUtil.get(
+					LocaleUtil.getDefault(),
+					"an-error-occurred-while-processing-the-requested-" +
+						"resource"));
+		}
+
+		response.setContentType(ContentTypes.APPLICATION_JSON);
+		response.setHeader(
+			HttpHeaders.CACHE_CONTROL,
+			HttpHeaders.CACHE_CONTROL_NO_CACHE_VALUE);
+
+		ServletResponseUtil.write(response, jsonObject.toString());
+
+		return null;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		WeDeployUserInfoAction.class);
+
+	@Reference
+	private WeDeployAuthTokenLocalService _weDeployAuthTokenLocalService;
+
+}

Isaji314/com-liferay-portal-security

portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/auth/publicpath/AuthPublicPath.java
@@ -23,7 +23,8 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"auth.public.path=/portal/wedeploy/access_token",
-		"auth.public.path=/portal/wedeploy/authorize"
+		"auth.public.path=/portal/wedeploy/authorize",
+		"auth.public.path=/portal/wedeploy/user"
 	},
 	service = Object.class
 )

portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/portlet/action/WeDeployUserInfoAction.java
@@ -1,0 +1,104 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.wedeploy.auth.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+import com.liferay.portal.kernel.struts.StrutsAction;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.security.wedeploy.auth.constants.WeDeployAuthTokenConstants;
+import com.liferay.portal.security.wedeploy.auth.model.WeDeployAuthToken;
+import com.liferay.portal.security.wedeploy.auth.service.WeDeployAuthTokenLocalService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Supritha Sundaram
+ */
+@Component(
+	immediate = true, property = {"path=/portal/wedeploy/user"},
+	service = StrutsAction.class
+)
+public class WeDeployUserInfoAction extends BaseStrutsAction {
+
+	@Override
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		String accessToken = ParamUtil.getString(request, "token");
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			WeDeployAuthToken weDeployAuthToken =
+				_weDeployAuthTokenLocalService.fetchTokenByType(
+					accessToken, WeDeployAuthTokenConstants.TYPE_AUTHORIZATION);
+
+			User user = _userLocalService.getUser(
+				weDeployAuthToken.getUserId());
+
+			JSONObject elementJSONObject = JSONFactoryUtil.createJSONObject();
+
+			elementJSONObject.put("email", user.getEmailAddress());
+			elementJSONObject.put("name", user.getFullName());
+
+			jsonObject.put("info", elementJSONObject);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			jsonObject.put(
+				"error_message",
+				LanguageUtil.get(
+					LocaleUtil.getDefault(),
+					"an-error-occurred-while-processing-the-requested-" +
+						"resource"));
+		}
+
+		response.setContentType(ContentTypes.APPLICATION_JSON);
+		response.setHeader(
+			HttpHeaders.CACHE_CONTROL,
+			HttpHeaders.CACHE_CONTROL_NO_CACHE_VALUE);
+
+		ServletResponseUtil.write(response, jsonObject.toString());
+
+		return null;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		WeDeployUserInfoAction.class);
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+	@Reference
+	private WeDeployAuthTokenLocalService _weDeployAuthTokenLocalService;
+
+}